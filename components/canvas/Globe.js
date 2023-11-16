13d2db5915e511e4fd886df27bb5c3a49ea49b9f
Bernardo Braga <bernardojbraga@gmail.com>
2023-11-16 12:06:31 -0300

    no mapping

--- components/canvas/Globe.js
+++ components/canvas/Globe.js
@@ -19,30 +19,56 @@ export default function Globe() {
     useEffect(() => {
         let phi = 0;
 
-        globe = createGlobe(canvasRef.current, {
-            devicePixelRatio: 1,
+        // globe = createGlobe(canvasRef.current, {
+        //     devicePixelRatio: 1,
+        //     width: 600 * 2,
+        //     height: 600 * 2,
+        //     phi: 0,
+        //     theta: 0,
+        //     dark: 0,
+        //     diffuse: 1.2,
+        //     mapSamples: 16000,
+        //     mapBrightness: 6,
+        //     baseColor: [1, 1, 1],
+        //     markerColor: [0.25, 1, 0.9],
+        //     glowColor: [1, 1, 1],
+        //     markers: [
+        //         // longitude latitude
+        //         { location: [37.7595, -122.4367], size: 0.03 },
+        //         { location: [40.7128, -74.006], size: 0.03 }
+        //     ],
+        //     offset: [675, 0],
+        //     onRender: (state) => {
+        //         // Called on every animation frame.
+        //         // `state` will be an empty object, return updated params.
+        //         state.phi = phi;
+        //         phi += 0.003;
+        //     }
+        // });
+
+        const globe = createGlobe(canvasRef.current, {
+            devicePixelRatio: 2,
             width: 600 * 2,
             height: 600 * 2,
             phi: 0,
             theta: 0,
-            dark: 0,
+            dark: 1,
             diffuse: 1.2,
             mapSamples: 16000,
             mapBrightness: 6,
-            baseColor: [1, 1, 1],
-            markerColor: [0.25, 1, 0.9],
-            glowColor: [1, 1, 1],
+            baseColor: [0.3, 0.3, 0.3],
+            markerColor: [0.1, 0.8, 1],
+            glowColor: [0, 0, 0],
             markers: [
                 // longitude latitude
                 { location: [37.7595, -122.4367], size: 0.03 },
-                { location: [40.7128, -74.006], size: 0.03 }
+                { location: [40.7128, -74.006], size: 0.1 }
             ],
-            offset: [675, 0],
             onRender: (state) => {
                 // Called on every animation frame.
                 // `state` will be an empty object, return updated params.
                 state.phi = phi;
-                phi += 0.003;
+                phi += 0.01;
             }
         });
 
