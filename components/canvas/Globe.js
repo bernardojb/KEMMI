dbfd3fa50e8d3e957a372e38eec1068332a9b4b8
Bernardo Braga <bernardojbraga@gmail.com>
2023-11-23 09:44:55 -0300

    no eventListener

--- components/canvas/Globe.js
+++ components/canvas/Globe.js
@@ -55,8 +55,8 @@ export default function Globe() {
                 { location: [10.774235974345606, 106.7049042559173], size: 0.05 }, //roche
 
             ],
-            scale: width < 750 ? 0.6 : 1,
-            offset: [width < 750 ? -400 : 500, 0],
+            scale:  1,
+            offset: [500, 0],
             onRender: (state) => {
                 // Called on every animation frame.
                 // `state` will be an empty object, return updated params.
@@ -82,59 +82,3 @@ export default function Globe() {
         </div>
     );
 }
-
-// export default function Globe() {
-//     const canvasRef = useRef();
-
-//     useEffect(()=>{
-//         const screen = window.innerWidth
-
-//         console.log(canvasRef)
-//         console.log("screen", screen)
-//     },[])
-
-//     useEffect(() => {
-//         let phi = 0;
-
-//         const globe = createGlobe(canvasRef.current, {
-//             devicePixelRatio: 2,
-//             width: 850 * 2,
-//             height: 850 * 2,
-//             phi: 0,
-//             theta: 0,
-//             dark: 0,
-//             diffuse: 1.2,
-//             mapSamples: 16000,
-//             mapBrightness: 6,
-//             baseColor: [1, 1, 1],
-//             markerColor: [0.25, 1, 0.9],
-//             glowColor: [1, 1, 1],
-//             markers: [
-//                 // longitude latitude
-//                 { location: [37.7595, -122.4367], size: 0.03 },
-//                 { location: [40.7128, -74.006], size: 0.03 }
-//             ],
-//             offset: [350 , 0 ],
-//             onRender: (state) => {
-//                 // Called on every animation frame.
-//                 // `state` will be an empty object, return updated params.
-//                 state.phi = phi;
-//                 phi += 0.003;
-//             }
-//         });
-
-//         return () => {
-//             globe.destroy();
-//         };
-//     }, []);
-
-//     return (
-//         <canvas
-//             ref={canvasRef}
-//             style={{ aspectRatio: 1 }}
-//             className="w-[850px] h-[850px] max-w-full"
-//         />
-//     );
-// }
-
-
