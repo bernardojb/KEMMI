598488d681aa30cc842e57ce333e9b81e6b80557
Bernardo Braga <bernardojbraga@gmail.com>
2023-11-23 12:01:45 -0300

    button mobile 3

--- components/buttons/DefaultButton.jsx
+++ components/buttons/DefaultButton.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const DefaultButton = ({ href, text, style, icon }) => {
     return (
         <Link href={href} className='whitespace-nowrap w-fit group'>
-            <div className={`py-[10px] sm:pb-[14px] sm:pt-[11px] px-7 rounded-[18px] ${style} w-fit flex flex-row flex-nowrap justify-center items-center`}> 
+            <div className={`py-[12px] sm:pb-[14px] sm:pt-[11px] px-7 rounded-[18px] ${style} w-fit flex flex-row flex-nowrap justify-center items-center`}> 
             {/* group-hover:transition-all ease-out duration-500 group-hover:translate-x-[4px]  */}
                 <h1 className='button-default font-neue-semibold text-base lg:text-lg mr-[10px]'>
                     {text}
